7aa3a9cceb2b2d3b153efd8052efb198123ae4ee
Ovidiu-R <ovidiu.m.radulescu@gmail.com>
2024-02-08 22:07:24 +0000

    Finish most of the features

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,21 +1,67 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 const canvas = document.getElementById('canvas');
 const draw = document.getElementById('draw');
-const cells = document.getElementById('cells');
+const gridSize = document.getElementById('gridSize');
 const backgroundColor = document.getElementById('backgroundColor');
 const trailColor = document.getElementById('trailColor');
 const trailDuration = document.getElementById('trailDuration');
 
-drawGrid(16);
+drawGrid(gridSize.value);
 
 draw.addEventListener ('click', () => {
-    drawGrid(cells.value);
+    drawGrid(gridSize.value);
 });
 
-function drawGrid(cellsPerSide) {
-    let cellSize = 960/cellsPerSide;
-    for (i=0; i<cellsPerSide; i++){
-        let newCell = document.createElement('div');
-        newCell.style.width = cellSize + 'px';
-        newCell.style.backgroundColor = 'yellow';
+function drawGrid(gridSize) {
+    let cellSize = 750/gridSize;
+    if (canvas.querySelectorAll('div.cell').length > 0){
+        clearCanvas();
+        drawGrid(gridSize);
+    } else {
+        for (let i=0; i<(gridSize**2); i++){
+            let newCell = document.createElement('div');
+            newCell.setAttribute('class', 'cell');
+            newCell.classList.add('cell');
+            newCell.style.width = cellSize + 'px';
+            newCell.style.height = cellSize + 'px';
+            newCell.style.backgroundColor = 'yellow';
+            setBackgroundColor(newCell);
+            canvas.appendChild(newCell);
+        } 
+        setTrailColor();
     }
 }
+
+function clearCanvas(){
+    let cells = canvas.querySelectorAll('div.cell');
+    cells.forEach(element => {
+        element.remove();
+    });
+}
+
+function setBackgroundColor(newCell){
+    newCell.style.backgroundColor = backgroundColor.value;
+}
+
+function setTrailColor(){
+    let newCells = canvas.querySelectorAll('.cell');
+    newCells.forEach (div => {
+        div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = trailColor.value;
+        });
+        div.addEventListener('mouseout', () => {
+            setTrailDuration(div, trailDuration.value);
+        });
+        
+    });
+}
+
+function setTrailDuration(div, trailDuration) {
+    setInterval(() => {
+        div.style.backgroundColor = backgroundColor.value;
+    }, (trailDuration*1000));
+    
+}
+
+});
