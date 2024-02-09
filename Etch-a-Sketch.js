30c14181bed7835e233dd5e4f08360d6e05990cc
Ovidiu-R <ovidiu.m.radulescu@gmail.com>
2024-02-09 14:24:30 +0000

    Add randomized trail color feature

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -6,11 +6,16 @@ const gridSize = document.getElementById('gridSize');
 const backgroundColor = document.getElementById('backgroundColor');
 const trailColor = document.getElementById('trailColor');
 const trailDuration = document.getElementById('trailDuration');
+const rainbowToggle = document.getElementById('random');
 
 drawGrid(gridSize.value);
 
 draw.addEventListener ('click', () => {
     drawGrid(gridSize.value);
+});
+
+rainbowToggle.addEventListener('click', () => {
+    rainbowToggle.classList.toggle('on');
 });
 
 function drawGrid(cellsPerSide) {
@@ -25,11 +30,11 @@ function drawGrid(cellsPerSide) {
     } else {
         for (let i=0; i<(cellsPerSide**2); i++){
             let newCell = document.createElement('div');
-            newCell.setAttribute('class', 'cell');
+            // newCell.setAttribute('class', 'cell');
             newCell.classList.add('cell');
             newCell.style.width = cellSize + 'px';
             newCell.style.height = cellSize + 'px';
-            newCell.style.backgroundColor = 'yellow';
+            // newCell.style.backgroundColor = 'yellow';
             setBackgroundColor(newCell);
             canvas.appendChild(newCell);
         } 
@@ -52,13 +57,26 @@ function setTrailColor(){
     let newCells = canvas.querySelectorAll('.cell');
     newCells.forEach (div => {
         div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = trailColor.value;
+            if (rainbowToggle.classList.contains('on')) {
+                randomizeColor(div);
+            } else {
+                div.style.backgroundColor = trailColor.value;
+            }
         });
         div.addEventListener('mouseout', () => {
             setTrailDuration(div, trailDuration.value);
         });
         
     });
+}
+
+function randomizeColor(div) {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i=0; i<6; i++) {
+        color += letters[Math.floor(Math.random() *16)];
+    }
+    div.style.backgroundColor = color;
 }
 
 function setTrailDuration(div, trailDuration) {
